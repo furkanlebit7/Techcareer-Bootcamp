afeebc6771bfba862b49dfa288a55e2a1fe6958e
furkanlebit7 <furkanlebitt7@gmail.com>
2023-08-25 03:43:15 +0300

    search input styled for dark mode

--- customer/src/components/CustomerTable/Search.js
+++ customer/src/components/CustomerTable/Search.js
@@ -7,7 +7,7 @@ const Search = ({ text, setText }) => {
         value={text}
         onChange={(e) => setText(e.target.value)}
         type="text"
-        className="w-full py-1 px-3 rounded-md focus:outline-blue-300"
+        className="w-full py-1 px-3 rounded-md focus:outline-none dark:text-gray-300  dark:bg-darkMain"
         placeholder="Search"
       />
     </div>
