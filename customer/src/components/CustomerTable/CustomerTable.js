025f0dfab0ca042072d95755d58966d5378d281b
furkanlebit7 <furkanlebitt7@gmail.com>
2023-08-25 03:27:07 +0300

    sort by all columns

--- customer/src/components/CustomerTable/CustomerTable.js
+++ customer/src/components/CustomerTable/CustomerTable.js
@@ -8,16 +8,26 @@ import { FaArrowsAltV } from "react-icons/fa";
 
 const CustomerTable = ({ customers, text }) => {
   const [isAscending, setIsAscending] = useState(true);
+  const [sortBy, setSortBy] = useState("id");
 
-  const handleOrder = () => {
+  const handleOrder = (sort) => {
+    setSortBy(sort);
     setIsAscending((prevIsAscending) => !prevIsAscending);
   };
 
   const sortedCustomers = [...customers].sort((a, b) => {
     if (isAscending) {
-      return a.companyName.localeCompare(b.companyName);
+      if (sortBy.startsWith("address.")) {
+        let sort = sortBy.split(".")[1];
+        return a.address[sort].localeCompare(b.address[sort]);
+      }
+      return a[sortBy].localeCompare(b[sortBy]);
     } else {
-      return b.companyName.localeCompare(a.companyName);
+      if (sortBy.startsWith("address.")) {
+        let sort = sortBy.split(".")[1];
+        return b.address[sort].localeCompare(a.address[sort]);
+      }
+      return b[sortBy].localeCompare(a[sortBy]);
     }
   });
 
@@ -37,18 +47,42 @@ const CustomerTable = ({ customers, text }) => {
     <table className="min-w-full bg-white shadow-md rounded-lg overflow-hidden dark:bg-darkBg">
       <thead className="bg-gray-200 text-gray-600 uppercase text-xs sm:text-sm dark:bg-darkMain dark:text-white">
         <tr>
-          <th className="py-3 px-4 font-semibold text-left">ID</th>
           <th className="py-3 px-4 font-semibold text-left">
-            Company Name
+            ID
             <button
               className="ml-2 text-sm text-gray-400"
-              onClick={handleOrder}
+              onClick={() => handleOrder("id")}
             >
               <FaArrowsAltV />
             </button>
           </th>
-          <th className="py-3 px-4 font-semibold text-left">Street</th>
-          <th className="py-3 px-4 font-semibold text-left">City</th>
+          <th className="py-3 px-4 font-semibold text-left">
+            Company Name
+            <button
+              className="ml-2 text-sm text-gray-400"
+              onClick={() => handleOrder("companyName")}
+            >
+              <FaArrowsAltV />
+            </button>
+          </th>
+          <th className="py-3 px-4 font-semibold text-left">
+            Street
+            <button
+              className="ml-2 text-sm text-gray-400"
+              onClick={() => handleOrder("address.street")}
+            >
+              <FaArrowsAltV />
+            </button>
+          </th>
+          <th className="py-3 px-4 font-semibold text-left">
+            City
+            <button
+              className="ml-2 text-sm text-gray-400"
+              onClick={() => handleOrder("address.city")}
+            >
+              <FaArrowsAltV />
+            </button>
+          </th>
         </tr>
       </thead>
       <tbody className="text-gray-600 dark:text-gray-200">
