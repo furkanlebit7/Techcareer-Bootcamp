d3c395a0d0901fe17d9991768d221f1085caafcf
furkanlebit7 <furkanlebitt7@gmail.com>
2023-08-30 21:24:02 +0300

    (LAyoUT) => Footer Section Completed

--- event-booking/src/Layouts/Footer/index.js
+++ event-booking/src/Layouts/Footer/index.js
@@ -1,7 +1,69 @@
 import React from "react";
 
+//ICONS
+import { BsFacebook, BsLinkedin } from "react-icons/bs";
+import { AiFillTwitterCircle } from "react-icons/ai";
+
 const Footer = () => {
-  return <div className="bg-indigo-900 h-[50vh]">Footer</div>;
+  return (
+    <div className="bg-indigo-900 text-whiteMain">
+      <div className="grid-cols-6 container  mx-auto grid  py-20">
+        <div className="col-start-1 col-end-3 flex flex-col gap-6">
+          <p className="text-2xl ">
+            <span className="font-bold text-3xl ">Event</span>
+            ick
+          </p>
+          <p>
+            Eventick is a global self-service ticketing platform for live
+            experiences that allows anyone to create, share, find and attend
+            events that fuel their passions and enrich their lives.
+          </p>
+          <div className="flex items-center gap-3 text-2xl">
+            <BsFacebook />
+            <AiFillTwitterCircle className="text-3xl" />
+            <BsLinkedin />
+          </div>
+        </div>
+        <div className="col-start-3 col-end-4 flex flex-col gap-6 justify-self-center">
+          <h4 className="text-xl font-bold">Plan Events</h4>
+          <ul className="flex flex-col gap-2 font-medium cursor-pointer">
+            <li>Create and Set Up </li>
+            <li>Sell Tickets</li>
+            <li>Online RSVP</li>
+            <li>Online Events</li>
+          </ul>
+        </div>
+        <div className="col-start-4 col-end-5 flex flex-col gap-6 justify-self-center">
+          <h4 className="text-xl font-bold">Eventick</h4>
+          <ul className="flex flex-col gap-2 font-medium cursor-pointer">
+            <li>About Us</li>
+            <li>Press</li>
+            <li>Contact Us</li>
+            <li>Help Center</li>
+            <li>How it Works</li>
+            <li>Privacy Terms</li>
+          </ul>
+        </div>
+        <div className="col-start-5 col-end-7 flex flex-col gap-6 justify-self-center">
+          <h4 className="text-xl font-bold">Stay in the loop</h4>
+          <p>
+            Join our mailing list to stay in the loop with our newest for Event
+            and concert
+          </p>
+          <div className="bg-white rounded-full py-2 px-3 flex items-center">
+            <input
+              type="text"
+              placeholder="Enter your email address.."
+              className="border-none flex-1 text-black  focus:ring-0"
+            />
+            <button className="bg-pinky py-2 px-3 rounded-full">
+              Subscribe Now
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
